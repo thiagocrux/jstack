9e404c927780c711046872f4c6648dc7130fc0dc
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-12-28 23:48:25 -0300

    feat:  implement category database operations

--- my-contacts/src/routes.js
+++ my-contacts/src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
@@ -9,5 +10,11 @@ router.post('/contacts', ContactController.store);
 router.get('/contacts/:id', ContactController.show);
 router.put('/contacts/:id', ContactController.update);
 router.delete('/contacts/:id', ContactController.delete);
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
+router.get('/categories/:id', CategoryController.show);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
