2961e970cf1c82080b43e18fe2820814d5a4afc0
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-12-29 00:16:58 -0300

    feat: add left join to all contact selection queries

--- my-contacts/src/app/repositories/ContactRepository.js
+++ my-contacts/src/app/repositories/ContactRepository.js
@@ -6,8 +6,10 @@ class ContactRepository {
 
     const rows = await db.query(
       `
-        SELECT * FROM contacts
-        ORDER BY name ${direction}
+        SELECT contacts.*, categories.name AS category_name
+        FROM contacts
+        LEFT JOIN categories ON categories.id = contacts.category_id
+        ORDER BY contacts.name ${direction}
       `,
     );
 
@@ -17,8 +19,10 @@ class ContactRepository {
   async findById(id) {
     const [row] = await db.query(
       `
-        SELECT * FROM contacts
-        WHERE id = $1
+        SELECT contacts.*, categories.name AS category_name
+        FROM contacts
+        LEFT JOIN categories ON categories.id = contacts.category_id
+        WHERE contacts.id = $1
       `,
       [id],
     );
