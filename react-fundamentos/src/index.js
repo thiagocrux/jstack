fb75bcc416c1efe11e2427400619b3cb1d07a722
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-12-30 15:17:37 -0300

    feat: create app component

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import App from "./App";
+
 /*  NOTE: This section of the course is not working for the versions installed,
  *  the refered documentation was https://react.dev/reference/react-dom/client/createRoot.
  */
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Olá, mundo!</h1>);
+root.render(<App />);
